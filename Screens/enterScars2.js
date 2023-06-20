d1814736fb4f446fba1828d088145560458b724c
Holden Schermer <holden@holdens-mbp.lan>
2023-06-19 15:07:19 -1000

    scar recognize progress

--- Screens/enterScars2.js
+++ Screens/enterScars2.js
@@ -3,7 +3,7 @@ let productButtons = document.querySelectorAll('button.btn');
 productButtons.forEach(productButton => {
   productButton.addEventListener('click', e => {
     let button = e.currentTarget;
-    productButtons.forEach(btn => btn !== button && btn.classList.remove('selected'));
+    productButtons.forEach(btn => btn !== button);
     button.classList.toggle('selected');
   });
 });
