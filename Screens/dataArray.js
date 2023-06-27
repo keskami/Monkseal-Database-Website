d458d2cf3c68c38767b749024472348e596ceba4
keskami <kkak808@gmail.com>
2023-06-26 15:40:42 -1000

    java

---
+++ Screens/dataArray.js
@@ -0,0 +1,7 @@
+
+function selectIsland() {
+    console.log("yes");
+    location.href='bleachNumber1.html';
+    var island = document.getElementsByClassName("selected").innerHTML;
+    console.log(island);
+}
