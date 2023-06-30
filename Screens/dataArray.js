1ba060954fcb6a7a2db5873996dcd7d701cee159
Holden Schermer <holden@holdens-mbp.lan>
2023-06-30 00:40:21 -1000

    workign search function need to figure out how to change pages still after it runs and how to make it display the found seals

--- Screens/dataArray.js
+++ Screens/dataArray.js
@@ -1,23 +1,34 @@
-let inputs = [null, null, null, null, null]
+let inputValues = [null, null, null, null, null]
+let inputKeys = [null, null, null, null, null]
 
 
 function selectIsland() {
+    let unsendableIslands = ["Hawai‘i", "Kaho‘olawe", "Kauai", "Lanai", "Moloka‘i", "Ni‘ihau", "Maui", "Oahu"]
+    let sendableIslands = ["hawaiisighting", "kahoolawesighting", "kauisighting", "lanaisighting", "molokaisighting", "niihausighting", "mauisighting", "oahusighting"]
     if (typeof (Storage) !== "undefined") {
-        localStorage.island = document.querySelector('.btn.generalBtn.selected').textContent
+        localStorage.islandKey = document.querySelector('.btn.generalBtn.selected').textContent
+        localStorage.islandValue = "x"
+        
+        for (i=0; i < sendableIslands.length; i++){
+            if (localStorage.islandKey == unsendableIslands[i]){
+                localStorage.islandKey = sendableIslands[i]
+            }
+        }
     } else {
         // Sorry! No Web Storage support..
     }
-    console.log(localStorage.island)
+    console.log(localStorage.islandKey)
     location.href = 'bleachNumber1.html';
 }
 
 function selectBleachNumber() {
     if (typeof (Storage) !== "undefined") {
-        localStorage.bleachNumber = document.querySelector('.bleachInput').value
+        localStorage.bleachKey = "sealid"
+        localStorage.bleachNumberValue = document.querySelector('.bleachInput').value
     } else {
         // Sorry! No Web Storage support..
     }
-    console.log(localStorage.bleachNumber);
+    console.log(localStorage.bleachNumberValue);
     location.href = 'recognizableScars1.html'
 }
 
@@ -28,11 +39,12 @@ function selectScarLocation() {
         for (i = 0; i < selected.length; i++) {
             id = id.concat(selected[i].id)
         }
-        localStorage.scarLocation = id
+        localStorage.scarLocationKey = id
+        localStorage.scarLocationValue = "x"
     } else {
         // Sorry! No Web Storage support..
     }
-    console.log(localStorage.scarLocation);
+    console.log(localStorage.scarLocationKey);
     location.href = 'naturalBleach1.html'
 }
 
@@ -43,40 +55,82 @@ function selectNaturalBleachLocation() {
         for (i = 0; i < selected.length; i++) {
             id = id.concat(selected[i].id)
         }
-        localStorage.naturalBleachLocation = id
+        localStorage.naturalBleachLocationKey = id
+        localStorage.naturalBleachLocationValue = "x"
     } else {
         // Sorry! No Web Storage support..
     }
-    console.log(localStorage.naturalBleachLocation);
+    console.log(localStorage.naturalBleachLocationKey);
     location.href = 'sealTags1.html'
 }
 
 
-
+async function getUser(jsonBody) {
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/monkseals/find', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonBody),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
 function results() {
     if (typeof (Storage) !== "undefined") {
-        localStorage.tagNumber= document.querySelector('.bleachInput').value
+        localStorage.tagNumberValue = document.querySelector('.bleachInput').value
+        localStorage.tagNumberKey = "lefttag"
     } else {
         // Sorry! No Web Storage support..
     }
-    console.log(localStorage.tagNumber);
+    console.log(localStorage.tagNumberValue);
 
-    inputs[0] = localStorage.island
-    inputs[1] = localStorage.bleachNumber
-    inputs[2] = localStorage.scarLocation
-    inputs[3] = localStorage.naturalBleachLocation
-    inputs[4] = localStorage.tagNumber
-    console.log(inputs)
-    location.href = 'searchResults.html'
-    //add code for grabbing data from database here. 
-    //the array for all results is in "input"
+    inputValues[0] = (localStorage.islandValue)
+    inputValues[1] = localStorage.bleachNumberValue
+    inputValues[2] = localStorage.scarLocationValue
+    inputValues[3] = localStorage.naturalBleachLocationValue
+    inputValues[4] = localStorage.tagNumberValue
+
+    inputKeys[0] = localStorage.islandKey
+    inputKeys[1] = localStorage.bleachKey 
+    inputKeys[2] = localStorage.scarLocationKey
+    inputKeys[3] = localStorage.naturalBleachLocationKey
+    inputKeys[4] = localStorage.tagNumberKey
+
+    
+
+    console.log(inputValues, inputKeys)
+    
 
 
-    inputs = [null, null, null, null, null]
+    var obj = {};
+    for (var i = 0; i < inputKeys.length; i++) {
+       obj[inputKeys[i]] = inputValues[i];
+    }
+
+    console.log(JSON.stringify(obj))
+    getUser(obj);
+
+
+    inputKeys = [null, null, null, null, null]
+    inputValues = [null, null, null, null, null]
+  
 }
 
 
 
 
+  
 
+
+  
--- Screens/test.js
+++ Screens/test.js
@@ -1,24 +1,32 @@
-async function getUser() {
 
-    try {
-      const response = await fetch('http://localhost:3000/api/v1/monkseals/', {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-        
-        },
-        
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      console.log( result);
-    } catch (err) {
-      console.log(err);
+
+
+async function getUser() {
+  try {
+    const response = await fetch('http://localhost:3000/api/v1/monkseals/find', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        hawaiisighting: 'x',
+        sealid: 'RK26',
+        dorsalscar: 'x',
+        dorsalbleach: 'x',
+        lefttag: 'K26'
+        // Add more key-value pairs as needed
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
     }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
   }
-  
-  getUser();
+}
+
+getUser();
