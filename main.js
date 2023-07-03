08801dd3879bcb4e4d26769d7db00769b6678a9e
keskami <kkak808@gmail.com>
2023-07-03 11:22:56 -1000

    consolidated

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+function myFunction() {
+    var x = document.getElementById("verticalMenu");
+    if (x.style.visibility === "visible") {
+      x.style.visibility = "hidden";
+      x.style.opacity = "0";
+      x.style.height = "0";
+    } else {
+      x.style.visibility = "visible";
+      x.style.opacity = "1";
+      x.style.height = "350px";
+    }
+  }
