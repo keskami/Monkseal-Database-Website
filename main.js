2a397630e0f8fd0cc919c2321288949846b36684
keskami <kkak808@gmail.com>
2023-07-14 15:27:56 -1000

    fun facts done

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
 function myFunction() {
-    var x = document.getElementById("verticalMenu");
-    if (x.style.height === "350px") {
-      x.style.opacity = "0";
-      x.style.height = "0";
-    
-    } else {
-      x.style.opacity = "1";
-      x.style.height = "350px";
-      
-    }
-  }
+  var x = document.getElementById("verticalMenu");
+  if (x.style.height === "350px") {
+    x.style.opacity = "0";
+    x.style.height = "0";
 
-function breathingFact() {
-  var circle = document.getElementById("breathingFact")
-  var modal = document.getElementById("breathingModal")
+  } else {
+    x.style.opacity = "1";
+    x.style.height = "350px";
+
+  }
+}
+
+function factOpen(type) {
+  var circle = document.getElementById(type + "Fact")
+  var modal = document.getElementById(type + "Modal")
+  var body = document.getElementById("body")
+
+  body.addEventListener('wheel', preventScroll);
 
   var offsetTop = circle.offsetTop
   var offsetLeft = circle.offsetLeft
   var scrollTop = window.scrollY
   var distanceTop = offsetTop - scrollTop
-  circle.style.transform = "translate(-" + (offsetLeft - 20) + "px,-" + (distanceTop - 20) +"px)"
+  circle.style.transform = "translate(-" + (offsetLeft - 20) + "px,-" + (distanceTop - 20) + "px)"
+  modal.style.transitionDelay = '1s'
+  modal.style.height = "100%"
+  circle.addEventListener('transitionend', function () {
+    circle.style.transitionDelay = '0.5s'
+    circle.style.transform = ''
+  });
+}
 
-  circle.addEventListener("transitionend", function(){
-    modal.style.height = "100%"
-    modal.style.opacity = "1"
-  })
+function returnFact(type) {
+  var circle = document.getElementById(type + "Fact")
+  var modal = document.getElementById(type + "Modal")
+  var body = document.getElementById("body")
+
+  body.removeEventListener('wheel', preventScroll);
+
+  modal.style.transitionDelay = ''
+  modal.style.height = "0"
+  circle.style.transitionDelay = '0s'
+}
+
+
+
+function preventScroll(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
 }
 
 function fullscreen() {
@@ -33,7 +58,7 @@ function fullscreen() {
   document.getElementById("expandBtn").style.display = "none"
 }
 
-function compressScreen(){
+function compressScreen() {
   document.getElementById("iframe").classList.remove("fullScreen")
   document.getElementById("expandBtn").style.display = "block"
   document.getElementById("compressBtn").style.display = "none"
