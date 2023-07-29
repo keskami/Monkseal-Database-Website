8d06c0ad046cd2ad33428c814bfdaaa141694c85
Holden Schermer <holden@holdens-mbp.lan>
2023-07-28 16:08:19 -1000

    added working search advance

--- Screens/searchForSeal.js
+++ Screens/searchForSeal.js
@@ -52,45 +52,72 @@ function bleachOpen() {
 function applyFilters() {
     closeModal()
     let inputs = []
+    let keys = [];
     if (typeof (Storage) !== "undefined") {
         localStorage.i = 0
         var islands = document.getElementById('islands')
         var tagNum = document.getElementById('tagNum')
+        var tagSide = document.getElementById('tagSide')
         var bleachNum = document.getElementById("bleachNum")
         var selectedScars = document.querySelectorAll('input[type="checkbox"]:checked')
         var selectedBleach = document.querySelectorAll('input[id="bleachDropdown"]:checked')
 
         if (islands.value != "") {
+            if (islands.value == "Hawaii"){
+                keys[localStorage.i] = "hawaiisighting"
+            }
+            if (islands.value == "Oahu"){
+                keys[localStorage.i] = "oahusighting"
+            }
             localStorage.i = parseInt(localStorage.i) + 1
-            localStorage.setItem("var" + localStorage.i, islands.value)
+            localStorage.setItem("var" + localStorage.i, "x")
         }
         if (tagNum.value.length != 0) {
+            keys[localStorage.i] = tagSide.value
             localStorage.i = parseInt(localStorage.i) + 1
             localStorage.setItem("var" + localStorage.i, tagNum.value)
+           
         }
         if (bleachNum.value.length != 0) {
+            keys[localStorage.i] = "sealid"
             localStorage.i = parseInt(localStorage.i) + 1
             localStorage.setItem("var" + localStorage.i, bleachNum.value)
+           
         }
 
         for (i = 0; i < selectedScars.length; i++) {
+            keys[localStorage.i] = selectedScars[i].value
             localStorage.i = parseInt(localStorage.i) + 1
-            localStorage.setItem("var" + localStorage.i, selectedScars[i].id)
+            localStorage.setItem("var" + localStorage.i, "x")
+            
         }
         for (i = 0; i < selectedBleach.length; i++) {
+            keys[localStorage.i] = selectedBleach[i].value
             localStorage.i = parseInt(localStorage.i) + 1
-            localStorage.setItem("var" + localStorage.i, selectedBleach[i].id)
+            localStorage.setItem("var" + localStorage.i, "x")
+            
         }
 
         for (l = 0; l < parseInt(localStorage.i); l++) {
             inputs[l] = localStorage.getItem("var" + (l + 1))
         }
+
+        console.log(keys)
         console.log(inputs)
+
+        const combinedObject = keys.reduce((obj, keys, index) => {
+            obj[keys] = inputs[index];
+            return obj;
+          }, {});
+        
+        getUser(combinedObject);
+
         localStorage.filterLength = inputs.length
     } else {
         // Sorry! No Web Storage support..
     }
     inputs = []
+    keys = []
 }
 
 function changeFilterNum() {
@@ -140,3 +167,33 @@ function clearModal() {
 
 }
 
+
+async function getUser(jsonBody) {
+
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/monkseals/find', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        mode: 'cors',
+        body: JSON.stringify(jsonBody),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      location.href = 'searchResults.html'
+      localStorage.results = JSON.stringify(result)
+      console.log(localStorage.results)
+  
+    } catch (err) {
+      console.log(err);
+    }
+  
+  
+  
+  }
